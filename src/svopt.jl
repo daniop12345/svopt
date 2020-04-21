19c36ec02c1c76daac7ca2a3dffbd83979bc16be
daniop <daniop12345@gmail.com>
2020-04-21 18:33:38 +0200

    add Newton Raphson method function

--- src/svopt.jl
+++ src/svopt.jl
@@ -1,8 +1,18 @@
 module svopt
 
-"Function calculate x^3 - x^2  + 2" 
-f(x) = x^3 - x^2 + 2
+"function minimize function with one variable using Newton_Raphson metod as arguments take function , derivative of this function and start point"
+function Newton_Raphson(f, df, x0; ϵ = 1e-9 )
+    new, old = x0, Inf
+    i = 0
+    while abs((new - old)/new) >= ϵ
+        x = new - (f(new)/df(new))
+        i = i + 1
+        new, old = x, new
+    end
+    return new
+end
 
-export f
+export Newton_Raphson
+
 
 end # module
