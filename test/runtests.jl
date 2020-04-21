7edd51c8acd449250eeca759fed19845a24ccea8
daniop <daniop12345@gmail.com>
2020-04-21 18:34:06 +0200

    add test to Newton_Raphson function

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,6 @@
 using svopt, Test
 
-@test f(5) == 102
+@testset "Newton_Raphson" begin
+    @test Newton_Raphson(x -> x^3 - 5x + 1, x -> 3x^2 - 5, 0) == 0.20163967572340463
+    @test Newton_Raphson(x -> exp(x) - x^4, x -> exp(x) - 4x^3, 2) == 1.4296118247255556
+end
